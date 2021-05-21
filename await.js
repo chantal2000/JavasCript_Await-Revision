627bd2c11c2f66417bf74750521278a127e7a1cb
chantal2000 <nniyonkuruchantal@gmail.com>
2021-05-21 21:46:46 +0300

    Await revision

--- await.js
+++ await.js
@@ -5,7 +5,9 @@ async function main(m, callback) {
     var m = await sum(3, 5)
     return m
 }
+// console.log(main(sum(3, 5), "God is good"))  when i Learn without this keyword then it gives me promise{<pending>} so; i think because it is promises i have to use it
+
 let c = main(sum(3, 5), "God is good")
 c.then(() => {
-        console.log(c)
-    })
+    console.log(c)
+})
